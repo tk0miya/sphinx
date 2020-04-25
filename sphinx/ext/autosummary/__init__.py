95cb4a52470154b995b820896e0ec4474dca8402
Takeshi KOMIYA <i.tkomiya@gmail.com>
2020-04-25 12:24:36 +0900

    Close #7549: autosummary: Enable autosummary_generate by default

--- sphinx/ext/autosummary/__init__.py
+++ sphinx/ext/autosummary/__init__.py
@@ -776,7 +776,7 @@ def setup(app: Sphinx) -> Dict[str, Any]:
     app.add_directive('autosummary', Autosummary)
     app.add_role('autolink', AutoLink())
     app.connect('builder-inited', process_generate_options)
-    app.add_config_value('autosummary_generate', [], True, [bool])
+    app.add_config_value('autosummary_generate', True, True, [bool])
     app.add_config_value('autosummary_generate_overwrite', True, False)
     app.add_config_value('autosummary_mock_imports',
                          lambda config: config.autodoc_mock_imports, 'env')
